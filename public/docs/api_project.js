1ff51df4d0971d685869091778ec595f686acee8
Icaro Caldeira <icaro.caldeira@hotmail.com>
2018-02-11 03:53:51 -0200

    add docs

---
+++ public/docs/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "Laureate places API",  "version": "1.0.0",  "description": "Laureate back-end test",  "title": "Laureate",  "url": "http://localhost:8080",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-02-11T05:51:02.675Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
